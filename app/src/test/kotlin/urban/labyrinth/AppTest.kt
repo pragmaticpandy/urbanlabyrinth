f54346ea5f0a722bc94404f0749350d75338bff3
Paul 💩 <paul@mustard.lol>
2021-04-25 00:24:29 -0700

    Remove dummy test.

--- app/src/test/kotlin/urban/labyrinth/AppTest.kt
+++ app/src/test/kotlin/urban/labyrinth/AppTest.kt
@@ -6,9 +6,4 @@ package urban.labyrinth
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
-    @Test fun testAppHasAGreeting() {
-        val classUnderTest = App()
-        assertNotNull(classUnderTest.greeting, "app should have a greeting")
-    }
-}
+class AppTest
